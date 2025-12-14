f970679354637a3c636537b3bd52935bdfc4e9ea
dinkelspiel <mail@keii.dev>
2025-12-14 08:06:58 +0100

    feat: add a destroy method for prairie_request_t

--- include/prairie/prairie.h
+++ include/prairie/prairie.h
@@ -2,7 +2,8 @@
 
 #ifndef PRAIRIE_H
 
-prairie_request_t *prairie_make_request(char *raw, int length);
+prairie_request_t *prairie_request_create(char *raw, int length);
+void prairie_request_destroy(prairie_request_t *request);
 
 #endif
 #define PRAIRIE_H
--- src/prairie.c
+++ src/prairie.c
@@ -1,11 +1,24 @@
+#include "stdlib.h"
 #include <prairie/lexer.h>
 #include <prairie/parser.h>
 
-prairie_request_t *prairie_make_request(char *raw, int length) {
+prairie_request_t *prairie_request_create(char *raw, int length) {
   prairie_token_t *token = prairie_lex(raw, length);
   //   print_tokens(token);
 
   prairie_request_t *request = prairie_parse_request(token);
   //   print_response(response);
   return request;
+}
+
+void prairie_request_destroy(prairie_request_t *request) {
+  prairie_header_t *header = request->header_start;
+
+  while (header != NULL) {
+    prairie_header_t *next = header->next;
+    free(header);
+
+    header = next;
+  }
+  free(request);
 }
